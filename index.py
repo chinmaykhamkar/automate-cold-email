66dc63d9e790d18e7063aa8e2c93c8f66017e376
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2024-02-03 01:15:57 +0530

    final commit

--- index.py
+++ index.py
@@ -19,33 +19,9 @@ smtp_password = os.getenv('SMTP_PASSWORD')
 
 email_body_template = """Hello {name},
 <br>
-Chinmay here, I'm writing this cold email to apply for any Software Engineering / Fullstack development position at {company}.
+I'm John Doe, writing this email to apply for xyx role at {company}.
 <br>
-A little about myself, I'm a full-stack developer and I'm passionate about coding. I like to solve real-world problems with the help of code. 
-<br>
-I'm always curious to learn about new technologies and challenge myself. I feel I'm a great team player as I always tend to prioritize team goals over personal achievements. 
-<br>
-One thing I can guarantee about myself is that I will always show up be it good days or bad, and I think you can't bet against this mindset.
-<br>
-I have prior experience working at startups and also at big-tech where I interned last summer at Amazon building scalable applications.
-<br>
-Take a look at one of my portfolio projects which was used by my college professors built using MERN stack - <a href="{portfolio_project_link}">Link</a>.
-<br>
-Few other side projects which I have built:
-<br>
-- Stackoverflow-chatpft-chrome-extension - <a href="{stackoverflow_chat_link}">Link</a> 
-<br>
-- YouTube-PDF-chatbot - <a href="{youtube_chatbot_link}">Link</a> 
-<br>
-My socials:
-<br>
-<a href="{linkedin_link}">LinkedIn</a> | <a href="{github_link}">GitHub</a> | <a href="{portfolio_link}">Portfolio</a>
-<br>
-Hoping to hear from you soon if there is a matching role.
-<br>
-Thanks and Regards,
-<br>
-Chinmay.
+your cold email body here
 """
 
 try:
@@ -58,17 +34,11 @@ try:
         email_body = email_body_template.format(
             name=row['First Name'], 
             company=row['Company'], 
-            portfolio_project_link="https://github.com/chinmaykhamkar/LoR",
-            stackoverflow_chat_link="https://github.com/chinmaykhamkar/chat-gpt-stackoverflow",
-            youtube_chatbot_link="https://github.com/chinmaykhamkar/youtube-pdf-chatbot",
-            linkedin_link="https://www.linkedin.com/in/chinmaykhamkar/",
-            github_link="https://github.com/chinmaykhamkar",
-            portfolio_link="https://chinmaykhamkar-github-io.vercel.app/"
             )
         message.attach(MIMEText(email_body, 'html'))
         
-        # pdf attach
-        pdf_path = "./Chinmay_Khamkar_resume.pdf"
+        # pdf attach (resume or other doc)
+        pdf_path = "./path to pdf.pdf"
         with open(pdf_path, "rb") as pdf_file:
             pdf_attachment = MIMEApplication(pdf_file.read(), _subtype="pdf")
             pdf_attachment.add_header('Content-Disposition', f'attachment; filename={pdf_path}')
